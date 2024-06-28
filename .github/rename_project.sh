f5f236d285ae94f34a87a8bec29370f9c0f787a0
xzyaoi <xzyaoi@users.noreply.github.com>
2024-06-28 14:46:55 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="FMStack"
+original_name="triteia"
+original_urlname="triteia"
+original_description="Awesome triteia created by FMStack"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv triteia $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
