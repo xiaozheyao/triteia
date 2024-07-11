f276d2d3a477c689fa176e950aba8bcb82533af9
Xiaozhe Yao <askxzyao@gmail.com>
2024-07-11 11:27:54 +0200

    packaging

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="Xiaozhe Yao",
-    packages=find_packages(exclude=["tests", ".github"]),
+    packages=find_packages(exclude=["tests", ".github", "benchmarks", "docs"]),
     install_requires=read_requirements("requirements.txt"),
     extras_require={"test": read_requirements("requirements-dev.txt")},
     ext_modules=[
